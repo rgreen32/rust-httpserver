83635823c071aa19115ffa262e1644d2e6e9413b
Mr.Bloosky <35779241+rgreen32@users.noreply.github.com>
2024-08-01 19:28:24 -0500

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener, string};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -12,6 +12,9 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(_stream) => {
+                let response: str = "HTTP/1.1 200 OK\r\n\r\n";
+                let repsonse_bytes =  response.as_bytes();
+                _stream.write(repsonse_bytes);
                 println!("accepted new connection");
             }
             Err(e) => {
