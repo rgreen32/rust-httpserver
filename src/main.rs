999ab62b98cce2b055cb75ebd9a4220f53e2b1f4
Mr.Bloosky <35779241+rgreen32@users.noreply.github.com>
2024-08-01 19:28:55 -0500

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(_stream) => {
-                let response: str = "HTTP/1.1 200 OK\r\n\r\n";
+                let response: &str = "HTTP/1.1 200 OK\r\n\r\n";
                 let repsonse_bytes =  response.as_bytes();
                 _stream.write(repsonse_bytes);
                 println!("accepted new connection");
