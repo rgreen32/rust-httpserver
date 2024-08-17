333a85c0500e214c53556d34ed524aadf6f13849
rgreen32 <ray.greenm@gmail.com>
2024-08-16 18:03:56 -0700

    adding tests

--- src/request_handler.rs
+++ src/request_handler.rs
@@ -5,18 +5,21 @@ use super::*;
 
 pub fn handle_request(request: HttpRequest) -> HttpResponse {
     
+    
     let response: HttpResponse = match request.request_target.as_str() {
         "/" => HttpResponse { 
                     version: request.version,
                     status_code: 200,
                     reason_phrase: String::from("OK"),
-                    headers: vec![] 
-                },
+                    headers: HashMap::new(),
+                    body: String::new()
+                }, 
         _ => HttpResponse {
                     version: request.version,
                     status_code: 404,
                     reason_phrase: String::from("Not Found"),
-                    headers: vec![]
+                    headers: HashMap::new(),
+                    body: String::new()
                 }
     };
 
@@ -79,5 +82,70 @@ pub fn get_request_line<Stream: BufRead>(stream: &mut Stream) -> Result<(String,
     }
 
     return  Ok(request_line);
+}
+
+pub fn read_stream_into_request<Stream: BufRead>(stream: &mut Stream) -> Result<HttpRequest, io::Error> {
+    let mut request_line_string = String::new();
+    let mut headers_string = String::new();
+    let mut body_string = String::new();
+
+    
+    let mut buffer = [0; 1024];
+    let mut value_position = 0;
+    let mut value_offset = 0;
+    let mut value_length = 0;
+
+    //request line
+    while value_position != 3 {
+        let bytes_read = stream.read(&mut buffer);
+        match bytes_read {
+            Ok(bytes_read) => {
+                if bytes_read == 0 {
+                    break;
+                }
+
+                for (index, byte) in buffer[..bytes_read].iter().enumerate() {
+                    if *byte == 13 && buffer[index+1] == 10  { // if value is \r\n
+                        let mut value = &buffer[value_offset..value_offset+value_length];
+                        match value_position {
+                            0 => {
+                                let _ = value.read_to_string(&mut request_line_string);
+                            },
+                            1 => {
+                                let _ = value.read_to_string(&mut headers_string);
+                            },
+                            2 => {
+                                let _ = value.read_to_string(&mut body_string);
+                            },
+                            _ => {
+                                panic!("First sequence line too big?");
+                            }
+                        }
+                        value_offset = value_offset+ value_length + 1;
+                        value_position += 1;
+                        value_length = 0;
+                    } else {
+                        value_length += 1;
+                    }
+                }
+            },
+            
+            Err(err) => {
+                return Err(err);
+            }
+        }
+
+    }
+
+    println!("request_line_string: {:?}", request_line_string);
+    
+    let request = HttpRequest { 
+                                    method: request_line_string,
+                                    request_target: String::new(),
+                                    version: String::new(),
+                                    headers: HashMap::new(),
+                                    body: String::new()
+                                };
+    return  Ok(request);
 }
 
--- tests/request_handler.rs
+++ tests/request_handler.rs
@@ -1,4 +1,4 @@
-use std::net::TcpStream;
+use std::{collections::HashMap, net::TcpStream};
 use std::io::Write;
 use std::str::from_utf8;
 use http_server_starter_rust::{request_handler, HttpRequest, HttpResponse};
@@ -20,14 +20,28 @@ fn test_get_request_line() {
     assert_eq!(version, "HTTP/1.1");
 }
 
+#[test]
+fn test_read_stream_into_request() {
+    // Mocking a TCP stream with in-memory data using Cursor
+    let request = b"HTTP/1.1 200 OK\r\nDate: Wed, 15 Aug 2024 12:00:00 GMT\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Length: 138\r\nConnection: keep-alive\r\n\r\n<html>\r\n<head><title>Sample Page</title></head>\r\n<body><h1>Hello, World!</h1></body>\r\n</html>\r\n";
+
+    let mut mock_stream = Cursor::new(request.to_vec());
+
+    let request: HttpRequest = request_handler::read_stream_into_request(&mut mock_stream).unwrap();
+
+    assert_eq!(request.method, "GET");
+    assert_eq!(request.request_target, "/");
+    assert_eq!(request.version, "HTTP/1.1");
+}
 
 #[test]
-fn test_service_request_root() {
+fn test_handle_request_root() {
     let request = HttpRequest {
                                     method: "GET".to_string(),
                                     request_target: "/".to_string(),
                                     version: "HTTP/1.1".to_string(),
-                                    headers: vec![],
+                                    headers: HashMap::new(),
+                                    body: String::new()
                                 };
 
     let response = request_handler::handle_request(request);
@@ -37,16 +51,52 @@ fn test_service_request_root() {
 }
 
 #[test]
-fn test_service_request_404() {
+fn test_handle_request_404() {
     let request = HttpRequest {
-                                    method: "GET".to_string(),
-                                    request_target: "/unknown".to_string(),
-                                    version: "HTTP/1.1".to_string(),
-                                    headers: vec![],
-                                };
+                    method: "GET".to_string(),
+                    request_target: "/unknown".to_string(),
+                    version: "HTTP/1.1".to_string(),
+                    headers: HashMap::new(),
+                    body: String::new()
+                };
 
     let response = request_handler::handle_request(request);
 
     assert_eq!(response.status_code, 404);
     assert_eq!(response.reason_phrase, "Not Found");
 }
+
+// #[test]
+// fn test_echo_path_returns_body_1(){
+
+//     let request = HttpRequest {
+//                     method: "GET".to_string(),
+//                     request_target: "/echo/abc".to_string(),
+//                     version: "HTTP/1.1".to_string(),
+//                     headers: HashMap::new(),
+//                     body: String::new()
+//                 };
+//     let response = request_handler::handle_request(request);
+    
+//     assert_eq!(response.status_code, 200);
+//     assert!(response.headers.contains_key("Content-Type"));
+//     assert!(response.headers.contains_key("Content-Length"));
+//     assert_eq!(response.body, "abc")
+// }
+
+// #[test]
+// fn test_echo_path_returns_body_2(){
+//     let request = HttpRequest {
+//                     method: "GET".to_string(),
+//                     request_target: "/echo/zzzzz_ff".to_string(),
+//                     version: "HTTP/1.1".to_string(),
+//                     headers: HashMap::new(),
+//                     body: String::new()
+//                 };
+//     let response = request_handler::handle_request(request);
+    
+//     assert_eq!(response.status_code, 200);
+//     assert!(response.headers.contains_key("Content-Type"));
+//     assert!(response.headers.contains_key("Content-Length"));
+//     assert_eq!(response.body, "abc")
+// }
